e76397be2259da9d2ee8edb6d7557b6a06f98b35
David Bellis <david.bellis1967@gmail.com>
2023-03-09 19:26:28 +0000

    added test for search_wiki function

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,4 +1,8 @@
 from mylib.logic import wiki
+from mylib.logic import search_wiki
 
 def test_wiki():
     assert "god" in wiki()
+
+def test_search_wiki():
+    assert any("Space Jam" in s for s in search_wiki("LeBron James"))
